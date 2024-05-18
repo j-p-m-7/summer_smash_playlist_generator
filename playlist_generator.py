dd56a62d659292667a3c0d14eadae946a9dada4d
Jack Monger <john.monger10@gmail.com>
2024-05-18 00:58:09 -0400

    working version of spotify api playlist

--- playlist_generator.py
+++ playlist_generator.py
@@ -11,52 +11,31 @@ from spotipy.oauth2 import SpotifyOAuth
 from datetime import datetime, timedelta
 from flask import Flask, redirect, request, jsonify, session
 
+# Loads environment variables
 load_dotenv()
 
+# Gets App information from .env
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 REDIRECT_URI = os.getenv('REDIRECT_URI')
 
-AUTH_URL = 'https://accounts.spotify.com/authorize'
-TOKEN_URL = 'https://accounts.spotify.com/api/token'
-API_BASE_URL = 'https://api.spotify.com/v1/'
-
+# Sets user scopes
 #scopes = "ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read user-read-email user-read-private user-soa-link user-soa-unlink soa-manage-entitlements soa-manage-partner soa-create-partner"
-
 scope = "user-read-currently-playing"
-
 scope = 'user-read-currently-playing user-read-private user-read-email playlist-read-private playlist-modify-public playlist-modify-private'
 
+# Initializes an instance of the Spotify class as an object
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                client_secret=CLIENT_SECRET,
                                                redirect_uri=REDIRECT_URI,
                                                scope=scope))
 
-# Gets artist from text 
-def get_artists_from_txt_file():
 
-    my_file = open("artists.txt", "r") 
-
-    data = my_file.read()  
-    artist_list = data.split("\n") 
-
-    my_file.close()
-
-    return artist_list
-
-def create_playlist():
-    # Create playlist
-    value = sp.user_playlist_create(1226802397, "Summer Smash - Spotify API")
-
-    # print(json.dumps(value, indent=4))
-
-    # print(value['id'])
-
-    return value['id']
-
+############## Testing ##############
 def filter_json():
     timestamp = "timestamp"
     context = "context"
+    pass
 
 
 
@@ -68,57 +47,121 @@ def filter_json():
 
     # Convert the filtered data dictionary back to a JSON string
     print(json.dumps(filtered_data, indent=4))
+############## Testing ##############
+
+
+# Gets artist from text 
+def get_artists_from_txt_file():
+
+    # Open file
+    my_file = open("artists.txt", "r") 
+    
+    # Puts artists in Python list
+    data = my_file.read()  
+    artist_list = data.split("\n") 
+
+    my_file.close()
+
+    return artist_list
 
 
 
 
-if __name__ == '__main__':
-    artist_list = get_artists_from_txt_file()
-    print(artist_list, '\n')
 
-    # value = sp.current_user()
-    # value = sp.current_user_playing_track()
-    # value = sp.playlist_add_items()
-    #print(json.dumps(value, indent=4))
-
-
-
+def create_artist_dict(artist_list):
     # Initialize an empty dictionary
     artist_dict = {}
 
     # Loop through artists
     for artist in artist_list:
 
+        # Search for artist name via Spotify Search
         value = sp.search(q=artist, type="artist", market="US")
 
+        # Get artist name and id
         artist_name = str(json.dumps(value["artists"]["items"][0]["name"], indent=4))[1:-1]
         artist_id = str(json.dumps(value["artists"]["items"][0]["id"], indent=4))[1:-1]
 
+        # Assign artist name and id as key value pairs in dictionary
         artist_dict[artist_name] = artist_id
 
-    print(artist_dict)
+    return artist_dict
 
-    # value = sp.search(q="Chief Keef", type="artist", market="US")
-    # print(json.dumps(value["artists"]["items"][0]["id"], indent=4))
-    # print(json.dumps(value["artists"]["items"][0]["name"], indent=4))
+def create_artist_tracks_dict(artist_dict):
 
-    # artist_name = str(json.dumps(value["artists"]["items"][0]["name"], indent=4))[1:-1]
-    # artist_id = str(json.dumps(value["artists"]["items"][0]["id"], indent=4))[1:-1]
-    # print(artist_id)
-    # print(artist_name)
+    # Initialize an empty dictionary
+    artist_tracks_dict = {}
 
-    # artist_dict[artist_name] = artist_id
+    # Iterate through artist name and artist id in dictionary
+    for key, value in artist_dict.items():
 
-    print(artist_dict)
- 
+        # Get artist top tracks
+        data = sp.artist_top_tracks(value)
+
+        # Assign tracks variable
+        tracks = data["tracks"]
+
+        # Initalize list
+        tracks_list = []
+
+        # For track in tracks, get uri and append to list
+        for track in tracks:
+            uri = track["uri"]
+            # value = sp.track(uri)["name"]
+            # print(value)
+            tracks_list.append(uri)
+
+        # Assign artist name and list of top 10 artist tracks as key value pairs in dictionary
+        artist_tracks_dict[key] = tracks_list
+
+    print(artist_tracks_dict)
+
+    return artist_tracks_dict
+
+def tracks_dict_to_list(tracks):
+    total_tracks = []
+
+    total_tracks = [y for x in tracks.values() for y in x]
+
+    return total_tracks
+
+def create_playlist(name):
+
+    # Create playlist
+    value = sp.user_playlist_create(1226802397, name)
+    print(json.dumps(value, indent=4))
+    sleep(100)
+
+    # Returns playlist id
+    return value['id']
+
+def add_tracks_to_playlist(playlist_id, list_tracks):    
+    n = 0
+    batch_size = 100
+
+    while n < len(list_tracks):
+        sp.playlist_add_items(playlist_id, list_tracks[n:n+batch_size])
+        n += batch_size
 
 
+if __name__ == '__main__':
+    artist_list = get_artists_from_txt_file()
+    print(artist_list, '\n')
 
+    artist_dict = create_artist_dict(artist_list)
+    print("\ngenerated artist_dict...\n")
 
-#value = sp.artist_top_tracks()
+    tracks_dict = create_artist_tracks_dict(artist_dict)
+    print("generated tracks_dict...\n")
 
+    tracks_list = tracks_dict_to_list(tracks_dict)
+    print("generated tracks_list...\n")
 
-# link = "https://open.spotify.com/playlist/5wjtNCVGUxBcS6MIX2sZQu?si=783fe4c7c53f4cfa&pt=f028bb50b95f357ed71fa9a9aadab1cb"
-
+    playlist_id = create_playlist("Summer Smash - Spotify API")
+    print("created playlist...\n")
+    
+    add_tracks_to_playlist(playlist_id, tracks_list)   
+    print("added tracks to playlist!\n")
+    
 
 
